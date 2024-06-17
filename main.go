7d98571f78d3dcb2c9622f353956da937efbabcf
ryota matsushita <ryotinjpn@gmail.com>
2024-06-17 23:22:49 +0900

    👍 main関数にStartMainServerを追加

--- main.go
+++ main.go
@@ -2,23 +2,12 @@ package main
 
 import (
 	"fmt"
+	"todo_app/app/controllers"
 	"todo_app/app/models"
-	"todo_app/config"
 )
 
 func main() {
-	fmt.Println(config.Config.Port)
-	fmt.Println(config.Config.SQLDriver)
-	fmt.Println(config.Config.DbName)
-	fmt.Println(config.Config.LogFile)
-
 	fmt.Println(models.Db)
 
-	u := &models.User{}
-	u.Name = "test"
-	u.Email = "test@example.com"
-	u.PassWord = "testtest"
-	fmt.Println(u)
-
-	u.CreateUser()
+	controllers.StartMainServer()
 }
