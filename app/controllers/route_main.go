c55feffbb050f34f6ec767d0dfacaf4d7ea17d40
ryota matsushita <ryotinjpn@gmail.com>
2024-06-19 12:14:14 +0900

    👍 generateHTMLを追加

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -1,15 +1,9 @@
 package controllers
 
 import (
-	"log"
 	"net/http"
-	"text/template"
 )
 
 func top(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("app/views/template/top.html")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	t.Execute(w, "Hello")
+	generateHTML(w, "Hello", "layout", "top")
 }
