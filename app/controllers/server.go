d3a0c1972f6df657ac45e515556539f360a5d0b7
ryota matsushita <ryotinjpn@gmail.com>
2024-06-19 13:29:02 +0900

    👍 login用ルーティングを設定

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -20,7 +20,9 @@ func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) 
 func StartMainServer() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
 	http.Handle("/static/", http.StripPrefix("/static/", files))
+
 	http.HandleFunc("/", top)
 	http.HandleFunc("/signup", signup)
+	http.HandleFunc("/login", login)
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
