b32e62ba1f598b2ca5127d019613ba255fddab61
ryota matsushita <ryotinjpn@gmail.com>
2024-06-21 11:44:36 +0900

    👍 未認証時のリダイレクト処理を追加

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -1,9 +1,30 @@
 package controllers
 
 import (
+	"log"
 	"net/http"
 )
 
 func top(w http.ResponseWriter, r *http.Request) {
-	generateHTML(w, "Hello", "layout", "public_navbar", "top")
+	_, err := session(w, r)
+	if err != nil {
+		generateHTML(w, nil, "layout", "public_navbar", "top")
+	} else {
+		http.Redirect(w, r, "/todos", http.StatusFound)
+	}
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	sess, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/", http.StatusFound)
+	} else {
+		user, err := sess.GetUserBySession()
+		if err != nil {
+			log.Println(err)
+		}
+		todos, _ := user.GetTodosByUser()
+		user.Todos = todos
+		generateHTML(w, user, "layout", "private_navbar", "index")
+	}
 }
